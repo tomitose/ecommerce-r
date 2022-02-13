90c7d1068c7bfdc3d2508c923de3c91e06301b76
tomitose <tomitosello@gmail.com>
2022-02-13 20:10:24 -0300

    Agregado nuevo producto a getFetch

--- src/components/services/getFetch.js
+++ src/components/services/getFetch.js
@@ -1,11 +1,12 @@
 const products = [
-    { id: 1, name: 'Nike', price: 200 },
-    { id: 2, name: 'Adidas', price: 180 },
-    { id: 3, name: 'Puma', price: 180 },
+    { id: 1, name: 'Nike', price: 200, stock: 10 },
+    { id: 2, name: 'Adidas', price: 180, stock: 6},
+    { id: 3, name: 'Puma', price: 180, stock: 4},
+    { id: 4, name: 'Reebok', price: 180, stock: 2},
 ];
 
 
-export const getFetch = new Promise((resolve, reject) => {
+export const getFetch = new Promise((resolve) => {
     setTimeout(() => {
         resolve(products);
     }, 2000);
