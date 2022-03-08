5ec56001e1088b9c30552e18edb09b41ec90eb17
tomitose <tomitosello@gmail.com>
2022-03-08 18:14:19 -0300

    Modificaciones generales

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -46,7 +46,7 @@ const ItemCount = ({initial, stock, onAdd}) => {
       { changeButton 
         ?  
         <Link to={"/cart"}>
-          <Button style={{alignSelf: "center", marginLeft:"30px"}}>Buy</Button>
+          <Button style={{alignSelf: "center", marginLeft:"30px"}}>Buy Now</Button>
         </Link>
         : <Button onClick={() => handlerOnAdd()} style={{alignSelf: "center", marginLeft:"30px"}}>Add to Cart</Button>
       }      
