f86bfb397a76eb2598fe3c9999ea92cd831ef084
tomitose <tomitosello@gmail.com>
2022-03-13 19:54:32 -0300

    Agregado funcion ItemDetailContainer para FireBase

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,21 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getFetch } from "../services/getFetch";
+//import { getFetch } from "../services/getFetch";
 import ItemDetail from "./ItemDetail/ItemDetail";
+import { getDoc,doc} from "firebase/firestore";
+import db from "../services/firebase";
 
 const ItemDetailContainer = () => {
     const [detail, setDetail] = useState([]);
 
     const {productId} = useParams();
 
-  useEffect(() => {
+    const getDataDetail = async (id) => {
+      try {
+        const document = doc(db, "items", id);
+        const resp = await getDoc(document);
+        const result = { id: resp.id, ...resp.data() };
+        setDetail(result);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
+  useEffect(() => {
+/*
     getFetch
     .then((response) => {
       setDetail(response.find( prod => prod.id === Number(productId) ));
     });
+*/
+  getDataDetail (productId)
 
   }, [productId]);
+
+
+  
+
 
   return (
     <div>
