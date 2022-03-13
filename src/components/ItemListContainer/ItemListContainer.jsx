8349f2a0db7015d6dfdea7a6b63491edf9c0dec3
tomitose <tomitosello@gmail.com>
2022-03-13 19:03:28 -0300

    Agregado de productos al Firebase

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,15 +1,55 @@
 import React from "react";
 import { useEffect,useState } from "react";
-import { useParams } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
+//import { useParams } from "react-router-dom";
 import ItemList from "../ItemList/ItemList";
-import { getFetch } from "../services/getFetch";
+//import { getFetch } from "../services/getFetch";
 import './ItemListContainer.css';
+import db from "../services/firebase";
 
 const ItemListContainer = () => {
   const [product, setProduct] = useState([]);
 
-  const {categoryId} = useParams();
+  
+  const getData = async () =>{
+    try {
+      const data = collection(db,"items")
+      const col = await getDocs(data)
+      const result = col.docs.map((doc) => doc ={id:doc.id,...doc.data()})
+      setProduct(result)
+    }
+    catch (error) {
+      console.log (error)
+    }
+  };
 
+  useEffect(() =>{
+    getData()
+  },[])
+
+  return (
+    <div className="Card-Item-List-Container">
+      <ItemList product={product}/>
+    </div>
+  )
+
+}
+
+export default ItemListContainer;
+
+
+
+
+
+
+
+
+
+
+
+  /*const {categoryId} = useParams();
+
+  
   useEffect(() => {
 
     if(categoryId) {
@@ -29,13 +69,4 @@ const ItemListContainer = () => {
     }
   },[categoryId])
     
-  
-
-  return (
-    <div className="Card-Item-List-Container">
-      <ItemList  product={product}/>
-    </div>
-  )
-
-  }
-export default ItemListContainer;
+  */
---
+++ src/components/services/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqX27JJE6Lb7-1-uWsfUu0joDn_FxU0SQ",
+  authDomain: "ecommercecalzadoreact.firebaseapp.com",
+  projectId: "ecommercecalzadoreact",
+  storageBucket: "ecommercecalzadoreact.appspot.com",
+  messagingSenderId: "921050844694",
+  appId: "1:921050844694:web:263537932bc0477942af37"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export default db;
